70a13cecdab4fc882fd9441943579a39e86696d4
ValeryShilov <valera.shilov.03@mail.ru>
2023-02-01 00:11:35 +0500

    1

--- Row sum.cpp
+++ Row sum.cpp
@@ -2,17 +2,23 @@
 #include <cmath>
 using namespace std;
 
+//double frec(int n) {
+//	if (n==1)
+//}
+
 double f(int x, int n)
 {
-	double S = 0;
+	double S = 0, k;
 	for (int i = 1; i <= n; i++) {
-		S += pow((-1), n + 1) * ((pow(x, 2 * n)) / 2 * n * (2 * n - 1));
+		k = pow((-1), i + 1) * ((pow(x, 2 * i)) / (2 * i * (2 * i - 1)));
+		S += k;
    }
 	return S;
 }
 
 int main() {
 	int x, n;
-	cin >> n >> x;
-	cout << f(x, n) << endl;
+	cin >> x >> n;
+	cout << f(x,n) << endl;
+	return 0;
 }
