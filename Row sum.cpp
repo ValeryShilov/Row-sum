1aa779090ba916940c4dc4f3e3792126c92433bc
ValeryShilov <valera.shilov.03@mail.ru>
2023-02-01 02:33:55 +0500

    work version1

--- Row sum.cpp
+++ Row sum.cpp
@@ -2,11 +2,16 @@
 #include <cmath>
 using namespace std;
 
-//double frec(int n) {
-//	if ()
-//}
+double f_rec(double x, int n) {   //вычисление суммы ряда с помощью рекурсии
+	double S = 0;
+	if (n == 1) return (x * x) / 2;
+	if (n > 1){
+		S += f_rec(x, n - 1) + pow((-1), n + 1) * ((pow(x, 2 * n)) / (2 * n * (2 * n - 1)));
+	}
+	return S;
+}
 
-double f(double x, int n)
+double f_с(double x, int n)  //вычисление суммы ряда с помощью цикла
 {
 	double S = 0, k;
 	for (int i = 1; i <= n; i++) {
@@ -19,10 +24,14 @@ double f(double x, int n)
 int main() {
 	setlocale(0, "");
 	int n;
-	double fn, x;
-	cin >> x >> n;
-	fn = x * atan(x) - log(sqrt(1 + x * x));
-	cout << "Вычисленное значение = " << f(x, n) << endl;
-	cout << "Значение функции = " << fn << endl;
+	double f, x;
+	do{
+		cout << "Введите -1<=x<=1 и номер члена ряда" << endl;
+		cin >> x >> n;
+	} while (x >= -1 || x <= 1);
+	f = x * atan(x) - log(sqrt(1 + x * x));  // функция из которой выведена последовательность
+	cout << "Вычисленное значение рекурсией = " << f_rec(x, n) << endl;
+	cout << "Вычисленное значение циклом = " << f_с(x, n) << endl;
+	cout << "Значение функции = " << f << endl;
 	return 0;
 }
