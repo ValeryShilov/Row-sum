0082a62f5897e216932f58e6a9af72886fc24678
ValeryShilov <valera.shilov.03@mail.ru>
2023-01-31 22:34:28 +0500

    Добавьте файлы проекта.

---
+++ Row sum.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double f(int x, int n)
+{
+	double S = 0;
+	for (int i = 1; i <= n; i++) {
+		S += pow((-1), n + 1) * ((pow(x, 2 * n)) / 2 * n * (2 * n - 1));
+   }
+	return S;
+}
+
+int main() {
+	int x, n;
+	cin >> n >> x;
+	cout << f(x, n) << endl;
+}
